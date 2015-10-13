e3997e912e165f287eb19cbac829ce73a81dfc6e
zhouzhizhao <626701841@qq.com>
2015-10-13 20:31:38 +0800

    my website

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+	has_secure_password
+
+	before_create { generate_token(:auth_token) }
+
+	validates :name, :email, presence: true
+              validates :name, :email, uniqueness: { case_sensitive: false }
+
+def generate_token(column)
+  begin
+    self[column] = SecureRandom.urlsafe_base64
+  end while User.exists?(column => self[column])
+end
+end
